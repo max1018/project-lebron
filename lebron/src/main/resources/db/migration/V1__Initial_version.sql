8c742d2f771d74d46a0faa72c3796183648aacb0
Brandon Seager <SeagerB@optimalinternet.com>
2018-07-20 12:26:47 -0400

    made changes to first flyway script

--- lebron/src/main/resources/db/migration/V1__Initial_version.sql
+++ lebron/src/main/resources/db/migration/V1__Initial_version.sql
@@ -1,5 +1,2 @@
-create database db_example;
-create user 'springuser'@'localhost' identified by 'ThePassword';
-grant all on db_example.* to 'springuser'@'localhost';
-insert into USER values(1, "bseager21@gmail.com", "Brandon Seager");
-
+CREATE TABLE user (id int, email varchar(255),Name varchar(255), Address varchar(255),City varchar(255) );
+insert into USER values(1, "bseager21@gmail.com", "Brandon Seager","123 up your ass street", "Grand Rapids MI");
